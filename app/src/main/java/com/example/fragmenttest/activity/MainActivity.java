24f208fec0e5d5b65995fd2a424b348281bfbbb0
yuanbao97 <316261393@qq.com>
2021-01-21 13:51:52 +0800

    4.2.2 + 4.2.3

--- app/src/main/java/com/example/fragmenttest/activity/MainActivity.java
+++ app/src/main/java/com/example/fragmenttest/activity/MainActivity.java
@@ -1,16 +1,43 @@
 package com.example.fragmenttest.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.fragmenttest.R;
+import com.example.fragmenttest.fragment.AnotherRightFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                replaceFragment(new AnotherRightFragment());
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
--- app/src/main/java/com/example/fragmenttest/fragment/AnotherRightFragment.java
+++ app/src/main/java/com/example/fragmenttest/fragment/AnotherRightFragment.java
@@ -1,21 +1,20 @@
 package com.example.fragmenttest.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.example.fragmenttest.R;
 
-public class RightFragment extends Fragment {
+public class AnotherRightFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.right_fragment, container, false);
+        View view = inflater.inflate(R.layout.another_right_fragment, container, false);
         return view;
     }
 
