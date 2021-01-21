a47340ff177dd85997523a0bbce4f44b790d81ac
yuanbao97 <316261393@qq.com>
2021-01-21 10:12:52 +0800

    4.2.1

---
+++ app/src/main/java/com/example/fragmenttest/fragment/LeftFragment.java
@@ -0,0 +1,21 @@
+package com.example.fragmenttest.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.fragmenttest.R;
+
+public class LeftFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left_fragment, container, false);
+        return view;
+    }
+}
